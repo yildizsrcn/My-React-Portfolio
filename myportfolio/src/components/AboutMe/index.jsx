9f59671e24f310d7691f723e6e1c6eed92dbcaed
Yusuf Sercan YILDIZ <srcnyildiz@Stars-MacBook-Pro.local>
2024-03-03 21:55:24 +0000

    Home section description and profile picture added.

--- myportfolio/src/components/AboutMe/index.jsx
+++ myportfolio/src/components/AboutMe/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./about.css"
-import profilepic from "../../assets/profilepic.jpg"
+import profilepic from "../../assets/srcn.jpg"
 
 
 export const AboutMe = () => {
@@ -8,11 +8,11 @@ export const AboutMe = () => {
     <div className='aboutmecontainer'>
        
         <div className='imagecontainer'>
-           <h1>about me</h1> 
+           <h1>About me</h1> 
             <img src={profilepic} alt="" />
         </div>
         <div className='content'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam consequuntur eligendi voluptatum eius distinctio accusantium facilis, non provident consectetur harum dolor saepe dolores sit voluptatibus quos aspernatur. Inventore, nemo pariatur.</p>
+            <p>I am an Industrial engineer who run his own food wholesaler company currently, I am planning to make a career changing soon end I will be completed my Front-End Web developer bootcamp soon. I am planning to add my previous knowledge and experience on to my new career .</p>
         </div>
     </div>
   )
