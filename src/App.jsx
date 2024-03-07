9a77fb00e7e1cfb3bd62e9e17a9a9db59a62e570
Yusuf Sercan YILDIZ <srcnyildiz@Stars-MacBook-Pro.local>
2024-03-07 22:05:37 +0000

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { AboutMe } from "./components/AboutMe"
-import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from "react-router-dom"
-import { Root } from "./components/Root"
-import { Work } from "./components/Work"
-import Resume  from "./components/Resume";
+import { AboutMe } from "./components/AboutMe";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Root } from "./components/Root";
+import { Work } from "./components/Work";
+import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 
 function App() {
-const router=createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root/>}>
-      <Route index element={<AboutMe/>}/>
-      <Route path="/work" element={<Work/>}/>
-      <Route path="/resume" element={<Resume /> }/> {'../../Resume/resume.pdf'}
-      <Route path="/contact" element={<Contact /> }/> 
-    </Route>
-
-  )
-);
   return (
     <>
-    <RouterProvider router={router}/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}>
+            <Route index element={<AboutMe />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/resume" element={<Resume />} />{" "}
+            {"../../Resume/resume.pdf"}
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
 
-export default App
+export default App;
